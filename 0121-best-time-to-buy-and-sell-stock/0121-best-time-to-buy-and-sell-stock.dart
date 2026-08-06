47f9bc99e8f6c55202f9c2c19caccacd0f903608
iYoussefAdly <youssefadly332@gmail.com>
2026-08-06 21:16:58 +0300

    Time: 6 ms (21.71%), Space: 188.8 MB (93.42%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.dart
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.dart
@@ -1,16 +1,16 @@
 class Solution {
   int maxProfit(List<int> prices) {
-    int minPrice = prices[0];
+    int lowest = prices[0];
     int maxProfit = 0;
-
     for (int i = 1; i < prices.length; i++) {
-      // Update the minimum price seen so far
-      if (prices[i] < minPrice) {
-        minPrice = prices[i];
+      var currentProfit = 0;
+      if (prices[i] < lowest) {
+        lowest = prices[i];
       } else {
-        // Profit if we sell today
-        int profit = prices[i] - minPrice;
-        if (profit > maxProfit) maxProfit = profit;
+        currentProfit = prices[i] - lowest;
+        if (currentProfit > maxProfit) {
+          maxProfit = currentProfit;
+        }
       }
     }
     return maxProfit;
